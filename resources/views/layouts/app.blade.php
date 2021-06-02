9df2c8ace9deeb1bbe4a9dfe61e1503ea153a89e
adventure-star <Boristolstyakov@protonmail.com>
2021-06-02 21:00:08 +0700

    favicon and title are changed

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,11 +3,11 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title>SuperSport</title>
+    <title>Sofa League</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Favicon -->
-    <link rel="shortcut icon" type="image/x-icon" href="img/favicon.ico">
+    <link rel="shortcut icon" type="image/x-icon" href="img/favicon.png">
     <!-- Fonts -->
     <link rel="stylesheet" href="{{asset('css/montserrat.css')}}">
     <!-- CSS -->
